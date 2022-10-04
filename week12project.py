56a8dcc09e32d7be15930589229ab4d890a58c95
Sean DuBois <seandubois45@gmail.com>
2022-10-04 19:36:32 +0000

    all done

---
+++ week12project.py
@@ -0,0 +1,15 @@
+services = []
+services.append("dynamoDB")
+services.append("ec2")
+services.append("vpc")
+services.append("s3")
+print(services)
+
+print("length of list:", len(services))
+
+services2 = services[:]
+services2.remove("vpc")
+services2.pop(1)
+print(services2)
+
+print("length of list:", len(services2))
